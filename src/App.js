fecb8aaa30535e04a60bb9a6a9a17a95309195b4
Edgar Khudoyan <khudoyanedgar@gmail.com>
2022-03-13 01:17:19 +0400

    create audio element and get via useRef

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
   return (
     <div className='App'>
       <Song currentSong={currentSong} />
-      <Player />
+      <Player currentSong={currentSong} />
     </div>
   );
 };
