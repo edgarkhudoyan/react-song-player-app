1e275926935609565eaa909311362f8ebd18828b
Edgar Khudoyan <khudoyanedgar@gmail.com>
2022-03-12 22:03:26 +0400

    install uuid

---
+++ src/util.js
@@ -0,0 +1,26 @@
+import { v4 as uuidv4 } from 'uuid';
+
+const ChillHop = () => {
+  return [
+    {
+      name: 'Beaver Creek',
+      cover: '',
+      artist: 'Aso, Middle School, Aviino',
+      audio: '',
+      color: ['#205950', '#2ab3bf'],
+      active: true,
+      id: '',
+    },
+    {
+      name: 'Beaver Creek',
+      cover: '',
+      artist: 'Aso, Middle School, Aviino',
+      audio: '',
+      color: ['#205950', '#2ab3bf'],
+      active: true,
+      id: '',
+    },
+  ];
+};
+
+export default ChillHop;
