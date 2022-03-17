fd0ee3a60577c65c8c1bb3ea0eacd358e52758af
Edgar Khudoyan <khudoyanedgar@gmail.com>
2022-03-18 00:22:20 +0400

    Customize slider and add styles

--- src/App.js
+++ src/App.js
@@ -20,14 +20,20 @@ const App = () => {
   const [songInfo, setSongInfo] = useState({
     currentTime: 0,
     duration: 0,
+    animationPercentage: 0,
   });
   const [libraryStatus, setLibraryStatus] = useState(false);
   // Event Handlers
   const timeUpdateHandler = (e) => {
     const current = e.target.currentTime;
     const duration = e.target.duration;
+    // Calculate Percentage
+    const roundedCurrent = Math.round(current);
+    const roundedDuration = Math.round(duration);
+    const animation = Math.round((roundedCurrent / roundedDuration) * 100);
 
-    setSongInfo({ ...songInfo, currentTime: current, duration: duration });
+
+    setSongInfo({ ...songInfo, currentTime: current, duration: duration, animationPercentage: animation });
   };
   return (
     <div className='App'>
